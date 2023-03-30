3017dc03d011a9065d5aa2f57f2e9ba7e2d3ed33
Yannic <yannic.erzinger@lernende.bbw.ch>
2023-03-30 16:33:48 +0200

    systemd

--- script/startup.sh
+++ script/startup.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
- docker run -d -p 9090:9090 -v /home/yannic/prometheus.yml:/etc/prometheus/ -v prom:/etc/prometheus/ --name prometheus-yannnnnnic prom/prometheus
- sudo docker run -d -p 3000:3000 --name grafana -v grafana:/etc/grafana/ grafana/grafana
+sudo docker run -d -p 9090:9090 -v /home/yannic/prometheus.yml:/etc/prometheus/ -v prom:/etc --name prometheus-yannnnnnic prom/prometheus
+sudo docker run -d -p 3000:3000 --name grafana -v grafana:/etc/grafana/ grafana/grafana
