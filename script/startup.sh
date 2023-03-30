775264a728288b822fe0b7be624bb9104b597287
Yannic <yannic.erzinger@lernende.bbw.ch>
2023-03-30 16:34:12 +0200

    ja

--- script/startup.sh
+++ script/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -d -p 9090:9090 -v /home/yannic/prometheus.yml:/etc/prometheus/ -v prom:/etc --name prometheus-yannnnnnic prom/prometheus
+sudo docker run -d -p 9090:9090 -v /home/yannic/prometheus.yml:/etc/ -v prom:/etc --name prometheus-yannnnnnic prom/prometheus
 sudo docker run -d -p 3000:3000 --name grafana -v grafana:/etc/grafana/ grafana/grafana
